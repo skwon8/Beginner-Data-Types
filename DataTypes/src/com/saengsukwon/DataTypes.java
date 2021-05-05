2bd2a45f377d04e4aa82f422d5d34aa30c7870c5
skwon8 <83226593+skwon8@users.noreply.github.com>
2021-05-04 20:07:56 -0400

    Add files via upload

---
+++ DataTypes/src/com/saengsukwon/DataTypes.java
@@ -0,0 +1,20 @@
+package com.saengsukwon;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+	char letter = 'S';
+	int number = 777;
+	float decimalNumber = 77.7f;
+	String name = "Harry Potter";
+	boolean result = true;
+
+        System.out.println("The Letter is " + letter);
+        System.out.println("My Favorite Number is " + number);
+        System.out.println("Degree of Weather is " + decimalNumber);
+        System.out.println("Main Character's name in My Favorite Movie is " + name);
+        System.out.println("The Answer is " + result);
+    }
+
+
+}
